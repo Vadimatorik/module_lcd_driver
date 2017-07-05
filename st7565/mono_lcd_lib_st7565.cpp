0016ffbe3875fdf9e4518fd9beef93aa869976fb
Vadimatorik <deryabkinda@mail.ru>
2017-07-05 13:47:03 +0700

    Почистил немного код.

--- st7565/mono_lcd_lib_st7565.cpp
+++ st7565/mono_lcd_lib_st7565.cpp
@@ -17,80 +17,39 @@ void mono_lcd_lib_st7565::set_brightness ( uint8_t val) const {
     this->com_out(CMD_SET_VOLUME_SECOND | (val & 0x3f));
 }
 
-void mono_lcd_lib_st7565::reset ( uint8_t contrast ) const {
-        uint8_t command;
-
-        cfg->cs->reset();
-        cfg->a0->reset();
-        cfg->res->set();
-        vTaskDelay(500);
+void mono_lcd_lib_st7565::reset ( uint8_t contrast ) const {        
         cfg->res->reset();
-        vTaskDelay(500);
+        USER_OS_DELAY_MS(50);
         cfg->res->set();
-        vTaskDelay(500);
-        command = 0xA2; this->spi->tx(&command, 1, 100);
-        command = 0xA0; this->spi->tx(&command, 1, 100);
-        command = 0xC8; this->spi->tx(&command, 1, 100);
-        command = 0x40; this->spi->tx(&command, 1, 100);
-        command = 0x2C; this->spi->tx(&command, 1, 100);
-        vTaskDelay(100);
-        command = 0x2E; this->spi->tx(&command, 1, 100);
-        vTaskDelay(100);
-        command = 0x2F; this->spi->tx(&command, 1, 100);
-        command = 0x26; this->spi->tx(&command, 1, 100);
-        command = 0x81; this->spi->tx(&command, 1, 100);
-        command = 0x1A; this->spi->tx(&command, 1, 100);
-        command = 0xAF; this->spi->tx(&command, 1, 100);
-        command = 0xB0; this->spi->tx(&command, 1, 100);
-        command = 0x10; this->spi->tx(&command, 1, 100);
-        command = 0x00; this->spi->tx(&command, 1, 100);
-        cfg->a0->set();
-        command = 0xFF; this->spi->tx(&command, 1, 100);
-        command = 0xFF; this->spi->tx(&command, 1, 100);
-        command = 0xFF; this->spi->tx(&command, 1, 100);
-        command = 0xFF; this->spi->tx(&command, 1, 100);
-        command = 0xFF; this->spi->tx(&command, 1, 100);
-        command = 0xFF; this->spi->tx(&command, 1, 100);
-        command = 0xFF; this->spi->tx(&command, 1, 100);
-        command = 0xFF; this->spi->tx(&command, 1, 100);
+        USER_OS_DELAY_MS(50);
 
-        /*
-        port_gpio_set(st7565_struct->cfg->pins_cs, 1, 100);
-        vTaskDelay(100);
-        cfg->cs->reset();
-        cfg->res->set();
-        vTaskDelay(100);
-        cfg->res->reset();
-        vTaskDelay(500);
-        cfg->res->set();
-        port_gpio_set(st7565_struct->cfg->pins_cs, 1, 100);
          // LCD bias select
-        _st7565_command(cfg, CMD_SET_BIAS_7);
+        this->com_out(CMD_SET_BIAS_9);
           // ADC select
-        _st7565_command(cfg, CMD_SET_ADC_NORMAL);
+        this->com_out(CMD_SET_ADC_NORMAL);
           // SHL select
-        _st7565_command(cfg, CMD_SET_COM_NORMAL);
+        this->com_out(CMD_SET_COM_NORMAL);
         // Initial display line
-        _st7565_command(cfg, CMD_SET_DISP_START_LINE);
+        this->com_out(CMD_SET_DISP_START_LINE);
 
          // turn on voltage converter (VC=1, VR=0, VF=0)
-        _st7565_command(cfg, CMD_SET_POWER_CONTROL | 0x4);
+        this->com_out(CMD_SET_POWER_CONTROL | 0x4);
          // wait for 50% rising
-        vTaskDelay(50);
+        USER_OS_DELAY_MS(50);
 
          // turn on voltage regulator (VC=1, VR=1, VF=0)
-        _st7565_command(cfg, CMD_SET_POWER_CONTROL | 0x6);
+        this->com_out(CMD_SET_POWER_CONTROL | 0x6);
          // wait >=50ms
-        vTaskDelay(50);
+        USER_OS_DELAY_MS(50);
 
          // turn on voltage follower (VC=1, VR=1, VF=1)
-        _st7565_command(cfg, CMD_SET_POWER_CONTROL | 0x7);
+        this->com_out(CMD_SET_POWER_CONTROL | 0x7);
          // wait
-        vTaskDelay(10);
+        USER_OS_DELAY_MS(10);
 
          // set lcd operating voltage (regulator resistor, ref voltage resistor)
-        _st7565_command(cfg, CMD_SET_RESISTOR_RATIO | 0x6);
-    */
+        this->com_out(CMD_SET_RESISTOR_RATIO | 0x6);
+
   this->com_out(CMD_DISPLAY_ON);
   this->com_out(CMD_SET_ALLPTS_NORMAL);
   this->set_brightness(contrast);
