e22a90ec488383dd8619c9d57eceedef41e8e564
Vadimatorik <deryabkinda@mail.ru>
2017-07-05 13:10:41 +0700

    Добавлен метод старта LCD (не тестировалось).

--- st7565/mono_lcd_lib_st7565.cpp
+++ st7565/mono_lcd_lib_st7565.cpp
@@ -4,3 +4,94 @@ void mono_lcd_lib_st7565::reinit (  const spi_base* spi_obj  ) const {
     this->mutex     = USER_OS_STATIC_MUTEX_CREATE( &this->mutex_buf );
     this->spi       = spi_obj;
 }
+
+void mono_lcd_lib_st7565::com_out ( uint8_t command ) const {
+    cfg->a0->reset();
+    cfg->cs->reset();
+    this->spi->tx( &command, 1, 100 );
+    cfg->cs->set();
+}
+
+void mono_lcd_lib_st7565::set_brightness ( uint8_t val) const {
+    this->com_out(CMD_SET_VOLUME_FIRST);
+    this->com_out(CMD_SET_VOLUME_SECOND | (val & 0x3f));
+}
+
+void mono_lcd_lib_st7565::reset ( uint8_t contrast ) const {
+        uint8_t command;
+
+        cfg->cs->reset();
+        cfg->a0->reset();
+        cfg->res->set();
+        vTaskDelay(500);
+        cfg->res->reset();
+        vTaskDelay(500);
+        cfg->res->set();
+        vTaskDelay(500);
+        command = 0xA2; this->spi->tx(&command, 1, 100);
+        command = 0xA0; this->spi->tx(&command, 1, 100);
+        command = 0xC8; this->spi->tx(&command, 1, 100);
+        command = 0x40; this->spi->tx(&command, 1, 100);
+        command = 0x2C; this->spi->tx(&command, 1, 100);
+        vTaskDelay(100);
+        command = 0x2E; this->spi->tx(&command, 1, 100);
+        vTaskDelay(100);
+        command = 0x2F; this->spi->tx(&command, 1, 100);
+        command = 0x26; this->spi->tx(&command, 1, 100);
+        command = 0x81; this->spi->tx(&command, 1, 100);
+        command = 0x1A; this->spi->tx(&command, 1, 100);
+        command = 0xAF; this->spi->tx(&command, 1, 100);
+        command = 0xB0; this->spi->tx(&command, 1, 100);
+        command = 0x10; this->spi->tx(&command, 1, 100);
+        command = 0x00; this->spi->tx(&command, 1, 100);
+        cfg->a0->set();
+        command = 0xFF; this->spi->tx(&command, 1, 100);
+        command = 0xFF; this->spi->tx(&command, 1, 100);
+        command = 0xFF; this->spi->tx(&command, 1, 100);
+        command = 0xFF; this->spi->tx(&command, 1, 100);
+        command = 0xFF; this->spi->tx(&command, 1, 100);
+        command = 0xFF; this->spi->tx(&command, 1, 100);
+        command = 0xFF; this->spi->tx(&command, 1, 100);
+        command = 0xFF; this->spi->tx(&command, 1, 100);
+
+        /*
+        port_gpio_set(st7565_struct->cfg->pins_cs, 1, 100);
+        vTaskDelay(100);
+        cfg->cs->reset();
+        cfg->res->set();
+        vTaskDelay(100);
+        cfg->res->reset();
+        vTaskDelay(500);
+        cfg->res->set();
+        port_gpio_set(st7565_struct->cfg->pins_cs, 1, 100);
+         // LCD bias select
+        _st7565_command(cfg, CMD_SET_BIAS_7);
+          // ADC select
+        _st7565_command(cfg, CMD_SET_ADC_NORMAL);
+          // SHL select
+        _st7565_command(cfg, CMD_SET_COM_NORMAL);
+        // Initial display line
+        _st7565_command(cfg, CMD_SET_DISP_START_LINE);
+
+         // turn on voltage converter (VC=1, VR=0, VF=0)
+        _st7565_command(cfg, CMD_SET_POWER_CONTROL | 0x4);
+         // wait for 50% rising
+        vTaskDelay(50);
+
+         // turn on voltage regulator (VC=1, VR=1, VF=0)
+        _st7565_command(cfg, CMD_SET_POWER_CONTROL | 0x6);
+         // wait >=50ms
+        vTaskDelay(50);
+
+         // turn on voltage follower (VC=1, VR=1, VF=1)
+        _st7565_command(cfg, CMD_SET_POWER_CONTROL | 0x7);
+         // wait
+        vTaskDelay(10);
+
+         // set lcd operating voltage (regulator resistor, ref voltage resistor)
+        _st7565_command(cfg, CMD_SET_RESISTOR_RATIO | 0x6);
+    */
+  this->com_out(CMD_DISPLAY_ON);
+  this->com_out(CMD_SET_ALLPTS_NORMAL);
+  this->set_brightness(contrast);
+}
--- st7565/mono_lcd_lib_st7565.h
+++ st7565/mono_lcd_lib_st7565.h
@@ -2,6 +2,53 @@
 
 #include "mono_lcd_lib_cfg.h"
 
+#define BLACK					1
+#define WHITE 					0
+
+#define LCDWIDTH 				128
+#define LCDHEIGHT 				64
+
+#define CMD_DISPLAY_OFF   		0xAE
+#define CMD_DISPLAY_ON    		0xAF
+
+#define CMD_SET_DISP_START_LINE 0x40
+#define CMD_SET_PAGE  			0xB0
+
+#define CMD_SET_COLUMN_UPPER  	0x10
+#define CMD_SET_COLUMN_LOWER  	0x00
+
+#define CMD_SET_ADC_NORMAL  	0xA0
+#define CMD_SET_ADC_REVERSE 	0xA1
+
+#define CMD_SET_DISP_NORMAL 	0xA6
+#define CMD_SET_DISP_REVERSE 	0xA7
+
+#define CMD_SET_ALLPTS_NORMAL 	0xA4
+#define CMD_SET_ALLPTS_ON  		0xA5
+#define CMD_SET_BIAS_9 			0xA2
+#define CMD_SET_BIAS_7 			0xA3
+
+#define CMD_RMW  				0xE0
+#define CMD_RMW_CLEAR			0xEE
+#define CMD_INTERNAL_RESET  	0xE2
+#define CMD_SET_COM_NORMAL  	0xC0
+#define CMD_SET_COM_REVERSE  	0xC8
+#define CMD_SET_POWER_CONTROL  	0x28
+#define CMD_SET_RESISTOR_RATIO  0x20
+#define CMD_SET_VOLUME_FIRST  	0x81
+#define CMD_SET_VOLUME_SECOND  	0
+#define CMD_SET_STATIC_OFF  	0xAC
+#define CMD_SET_STATIC_ON  		0xAD
+#define CMD_SET_STATIC_REG  	0x0
+#define CMD_SET_BOOSTER_FIRST  	0xF8
+#define CMD_SET_BOOSTER_234  	0
+#define CMD_SET_BOOSTER_5  		1
+#define CMD_SET_BOOSTER_6  		3
+#define CMD_NOP  				0xE3
+#define CMD_TEST  				0xF0
+
+#define ST7565_STARTBYTES 		1
+
 /*
  * Парметры SPI MOTOROLA, CPHA = 1, CPOL = 1.
  */
@@ -20,8 +67,11 @@ class mono_lcd_lib_st7565 {
 public:
     constexpr mono_lcd_lib_st7565 ( const mono_lcd_lib_st7565_cfg_t* const cfg, uint8_t* const buf ): cfg(cfg), buf(buf) {}
 
-    void    reinit          ( const spi_base* spi_obj ) const;
 
+    void    reinit          ( const spi_base* spi_obj ) const;
+    void  reset ( uint8_t contrast ) const;
+    void com_out ( uint8_t command ) const;
+    void set_brightness ( uint8_t val) const;
 private:
 
     mutable bool flag = false;  // Инициализирован ли LCD?
